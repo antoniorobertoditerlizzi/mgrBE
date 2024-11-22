4998b06c441b31b269069db6dc09153d4e75e8ce
antoniorobertoditerlizzi <133643377+antoniorobertoditerlizzi@users.noreply.github.com>
2024-11-22 14:33:02 +0100

    gestione del Bearer Token all'interno delle chiamate autorizzate dallo swagger
    gestione del Bearer Token all'interno delle chiamate autorizzate dallo swagger

--- src/main/java/com/vigilfuoco/mgr/SwaggerConfig.java
+++ src/main/java/com/vigilfuoco/mgr/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.vigilfuoco.mgr;
 
 import java.util.Collections;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,13 +9,16 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 /* 
- * Configurazione dei parametri per la generazione dello SWAGGER automatico
+ * Configurazione dei parametri per la generazione dello SWAGGER automatico. Per le chiamate autorizzate aggiungere "Bearer " nello swagger
  * 
  */
 
@@ -29,7 +33,9 @@ public class SwaggerConfig {
 	          .apis(RequestHandlerSelectors.any())              
 	          .paths(PathSelectors.any())                          
 	          .build()
-	          .apiInfo(apiInfo());
+	          .apiInfo(apiInfo())
+	          .securitySchemes(Collections.singletonList(apiKey())) // Supporto per il Bearer Token
+	          .securityContexts(Collections.singletonList(securityContext())); // Configurazione di sicurezza
 	    }
 	    
 	    private ApiInfo apiInfo() {
@@ -42,4 +48,21 @@ public class SwaggerConfig {
 	          "License of API", "API license URL", Collections.emptyList());
 	    }
 	
-}
+	    // Definizione dell'ApiKey per il Bearer Token
+	    private ApiKey apiKey() {
+	        return new ApiKey("Bearer Token", "Authorization", "header");
+	    }
+
+	    // Configurazione del contesto di sicurezza
+	    private SecurityContext securityContext() {
+	        return SecurityContext.builder()
+	                .securityReferences(defaultAuth())
+	                .build();
+	    }
+
+	    private List<SecurityReference> defaultAuth() {
+	        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+	        AuthorizationScope[] authorizationScopes = new AuthorizationScope[]{authorizationScope};
+	        return Collections.singletonList(new SecurityReference("Bearer Token", authorizationScopes));
+	    }
+	}
